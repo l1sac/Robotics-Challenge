9f082862bd4c8ea2ae1a22381aef21d53cfa56bc
l1sac <meihuan.chen@education.nsw.gov.au>
2025-03-26 12:45:06 +1100

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+
 from hub import light_matrix
 from motor import velocity
 import runloop
@@ -18,22 +19,23 @@ def follow_line():
         if color_sensor.color(port.B) == color.BLACK:
             motor_pair.move(motor_pair.PAIR_1, 0, velocity=280)
         else:
-            time.sleep_ms(2)
-            motor_pair.move(motor_pair.PAIR_1, -90, velocity=140)
+            time.sleep_ms(15)
+            motor_pair.move_for_degrees(motor_pair.PAIR_1, -90, -90, velocity=280)
+            time.sleep_ms(20)
+
+        global score
+        if color_sensor.color(port.B) == color.GREEN:
+            score += 1
+            light_matrix.write(str(score))
 
 # time.sleep(0.05) can put it to ensure accuracy
 
-def square_counter():
-    global score
-    if color_sensor.color(port.B) == color.GREEN:
-        light_matrix.write("!")
-        score += 1
-        light_matrix.write(str(score))
-
+#def square_counter():
+    
 async def main():
     while True:
         follow_line()
-        square_counter()
-        await runloop.sleep_ms(50)
+        #square_counter()
+        #await runloop.sleep_ms(50)
 
 runloop.run(main())
