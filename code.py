14d1b6cef4a00bd9a2a7a6f444b60eaf5bb05f86
l1sac <meihuan.chen@education.nsw.gov.au>
2025-04-01 19:08:05 +1100

    test 1

--- code.py
+++ code.py
@@ -1,37 +1,26 @@
-from hub import light_matrix
+from hub import light_matrix, port
 from motor import velocity
 import runloop
 import motor_pair
-from hub import port
 import color_sensor
-import color
-import motor
 import time
 
-score = 0
 motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
+color_sensor_port = port.B  # Ensure this is the correct port
+
+BLACK_THRESHOLD = 45  # Adjust this value
+BASE_SPEED = 280
+PROPORTIONAL_CONSTANT = 1.0  # Adjust this value
 
 async def main():
-    global score
     while True:
-        reflection = color_sensor.reflection(port.B)
+        reflection = color_sensor.reflection(color_sensor_port)
 
-        if reflection < 45:
-            motor_pair.move(motor_pair.PAIR_1, 0, velocity=280)
-        elif 45 < reflection < 60:
-            score += 1
-            light_matrix.write(str(score))
-        else:
-            
-            await motor_pair.move_for_degrees(motor_pair.PAIR_1, 60, -90, velocity=100 )
+        error = reflection - BLACK_THRESHOLD
+        steering_adjustment = int(error * PROPORTIONAL_CONSTANT)
 
-            if color_sensor.reflection(port.B) < 45:
-                continue
+        motor_pair.move(motor_pair.PAIR_1, 0, velocity=BASE_SPEED, steering=steering_adjustment)
 
-            await motor_pair.move_for_degrees(motor_pair.PAIR_1, -180, -90, velocity=100 )
-            
-            if color_sensor.reflection(port.B) < 45:
-                continue
         await runloop.sleep_ms(5)
 
 runloop.run(main())
