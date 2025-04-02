a4942ca81ad50babc7f4db8a846bfd53e5fc5dea
k493-g <kayelynn.khaw@education.nsw.gov.au>
2025-04-02 13:26:44 +1100

    Update code.py (work in progress)

--- code.py
+++ code.py
@@ -7,19 +7,21 @@ import time
 
 # Initialization
 motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
+light_matrix.write("!")
 color_sensor_port = port.B
 score = 0
+color = 6
 async def main():
     while True:
         global score
-        if color_sensor.color(port.B) == color.GREEN:
+        global color
+        if color_sensor.color(port.B) == color:
             motor_pair.stop(motor_pair.PAIR_1)
-            light_matrix.write("!")
-            await runloop.sleep_ms(2000)# Stop for 2 seconds
             score += 1
             light_matrix.write(str(score))
+            await runloop.sleep_ms(2000)
         else:
-            motor_pair.move_tank_for_time(motor_pair.PAIR_1, 1000, 1000, 5000)
+            motor_pair.move_tank_for_time(motor_pair.PAIR_1, 1000, 1000, 500)
         await runloop.sleep_ms(5)
 
 runloop.run(main())
