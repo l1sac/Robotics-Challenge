18365395fa98544bbb0b7e7728fccd7107636f10
l1sac <meihuan.chen@education.nsw.gov.au>
2025-04-07 18:07:49 +1000

    please work

--- code.py
+++ code.py
@@ -4,28 +4,30 @@ import runloop
 import motor_pair
 from hub import port
 import color_sensor
+import distance_sensor
 import color
 import motor
 import time
-
+from hub import sound
 score = 0
 motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
 
 async def main():
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 5000,0 , velocity=280)
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 90, 0, velocity=280)
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 15000, 0, velocity= 280)
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 90, 0, velocity=280)
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 2500,0 , velocity=280)
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 90, 0, velocity=280)
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 12500, 0, velocity= 280)
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 90, 0, velocity=280)
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 2500,0 , velocity=280)
+    global score
     while True:
-        global score
-        if color_sensor.color(port.B) == color.RED:
-            motor_pair.stop(motor_pair.PAIR_1)
-        elif color_sensor.color(port.B) == color.GREEN:
+        reflection = color_sensor.reflection(port.B)
+        if reflection < 25: # if black it will turn in one direction
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 100, 50)
+        elif 70 < reflection < 100: # if white it will turn in the other direction
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 50, 100)
+        elif 30 < reflection < 60: # score counter
             score += 1
             light_matrix.write(str(score))
+            await runloop.sleep_ms(5000)
+        elif distance_sensor.distance(port.A) < 800: # beep when the distance is less than 8 cm
+            sound.beep(100, 500, 25)
+        else: # if detect anything else other than white, black and red (green) it will stop and beep
+            motor_pair.stop(motor_pair.PAIR_1)
+            sound.beep(440, 500, 100)
+            await runloop.sleep_ms(10000)
 runloop.run(main())
