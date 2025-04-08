5f9b125fe33cfd34dae5e95467c5a46a608f260f
l1sac <meihuan.chen@education.nsw.gov.au>
2025-04-08 18:17:42 +1000

    needs to be tested

--- code.py
+++ code.py
@@ -13,8 +13,9 @@ score = 0
 motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
 detection_threshold = 5
 detection_count = 0
+was_on_green = False
 async def main():
-    global score, detection_count
+    global score, detection_count, was_on_green
     while True:
         if distance_sensor.distance(port.A) < 800: # beep when the distance is less than 8 cm
             detection_count += 1
@@ -23,14 +24,18 @@ async def main():
                 detection_count = 0
 
         if color_sensor.color(port.B) == color.BLACK: # if black it will turn in one direction
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 100, 50)
+             motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 40, 100, 60)
 
         elif color_sensor.color(port.B) == color.WHITE: # if white it will turn in the other direction
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 50, 100)
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 40, 60, 100)
 
-        if color_sensor.color(port.B) == color.GREEN: # score counter
-            score += 1
-            light_matrix.write(str(score))
+        if color_sensor.color(port.B) == color.GREEN:
+            if not was_on_green:
+                score += 1
+                light_matrix.write(str(score))
+                was_on_green = True
+        else:
+            was_on_green = False
 
         if color_sensor.color(port.B) == color.RED : # if detect anything else other than white, black and red (green) it will stop and beep
             motor_pair.stop(motor_pair.PAIR_1)
