e4aeeb1492bdd996e689f1f73e45f7cf0af1cc3b
k493-g <kayelynn.khaw@education.nsw.gov.au>
2025-04-09 12:41:19 +1000

    slight changes in speed

--- code.py
+++ code.py
@@ -21,7 +21,7 @@ was_on_green = False
 async def main():
     global score, detection_count, was_on_green
     while True:
-        motor.run_for_time(port.E, 500, 300)
+        motor.run_for_time(port.E, 10000, 500)
         if distance_sensor.distance(port.A) < 150: # detect when the distance is less than 15 cm
             detection_count += 1
             if detection_count > 5: # only beeps (substitute for quack sound) when the detection_count is above 5
@@ -29,19 +29,19 @@ async def main():
                 detection_count = 0 # resets the detection_count
 
         if color_sensor.color(port.B) == color.BLACK: # if it senses black it will turn right
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 40, 100, 60)
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 40, 160, 120)
 
         elif color_sensor.color(port.B) == color.WHITE: # if it senses white it will turn left
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 30, 60, 100)
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 40, 120, 160)
 
         if color_sensor.color(port.B) == color.GREEN: # score counter
             if not was_on_green:
                 score += 1
                 light_matrix.write(str(score))
+                #keeps moving after it senses green
+                motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 0, 80, 80)
                 was_on_green = True
-                #keeps moving after it senses green (in progress)
-                motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 0, 50, 50)
-                #stops after sensing a certain amount of green squares (in progress)
+                #stops after sensing a certain amount of green squares
                 if score == 10:
                     light_matrix.clear()
         else:
