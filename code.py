935b030a0196b94e7224c5f91e4dfd915ad4c414
l1sac <meihuan.chen@education.nsw.gov.au>
2025-04-27 20:33:49 +1000

    proportionate line following needs to be tested

--- code.py
+++ code.py
@@ -8,52 +8,45 @@ import distance_sensor
 import color
 import motor
 import time
-from hub import sound
+from app import sound
 
+global score, detection_count, was_on_green
 # variables
 score = 0
-motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
 detection_threshold = 5
 detection_count = 0
 was_on_green = False
+movement_motors = motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
+target_reflection = 50 # adjust needs to be the average between white n black
+KP = 1.0
+base_speed = 40
+reflection = color_sensor.reflection(port.B)
 
-# main loop
-async def main():
-    global score, detection_count, was_on_green
-    while True:
-        motor.run_for_time(port.E, 10000, 500)
-        if distance_sensor.distance(port.A) < 150: # detect when the distance is less than 15 cm
+while True: 
+    motor.run_for_time(port.E, 10000, 500) # can knocker
+    error = reflection - target_reflection
+    turn_rate = int(KP * error)
+    if distance_sensor.distance(port.A) < 150: # detect when the distance is less than 15 cm
             detection_count += 1
             if detection_count > 5: # only beeps (substitute for quack sound) when the detection_count is above 5
-                await sound.beep(200, 500, 25)
+                sound.play('Squeaky Toy', 80, 50)
                 detection_count = 0 # resets the detection_count
 
-        if color_sensor.color(port.B) == color.BLACK: # if it senses black it will turn right
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 30, 140, 100)
-
-        elif color_sensor.color(port.B) == color.WHITE: # if it senses white it will turn left
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 30, 100, 140)
-
-        if color_sensor.color(port.B) == color.GREEN: # score counter
-            if not was_on_green:
+    
+    if 30 < reflection < 60: #needs adjustment, this is for green square counter 
+        if not was_on_green:
                 score += 1
                 light_matrix.write(str(score))
                 #keeps moving after it senses green
-                motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 0, 80, 80)
+                motor_pair.move(motor_pair.PAIR_1, turn_rate, velocity=base_speed)
                 was_on_green = True
                 #stops after sensing a certain amount of green squares
                 if score == 11:
                     light_matrix.clear()
-                    
-        else:
-            was_on_green = False
-
-        if color_sensor.color(port.B) == color.RED : # if the colour sensed is red it will stop and play a success tune
-            motor_pair.stop(motor_pair.PAIR_1)
-            await sound.beep(440, 500, 100)
-            await sound.beep(500, 500, 100)
-            await sound.beep(440, 500, 100)
-            await sound.beep(500, 500, 100)
-            await sound.beep(440, 500, 100)
-            await runloop.sleep_ms(10000)
-runloop.run(main())
+    
+    if < reflection < : # if the colour sensed is red it will stop and play a success tune
+        motor_pair.stop(motor_pair.PAIR_1)
+        sound.play('Emotional Piano')
+    else:
+        motor_pair.move(motor_pair.PAIR_1, turn_rate, velocity=base_speed)
+    
