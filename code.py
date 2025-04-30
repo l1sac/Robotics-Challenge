513c76bf6ef37758977b4ec079f4ae4187601c4e
l1sac <meihuan.chen@education.nsw.gov.au>
2025-04-30 12:03:08 +1000

    need to test

--- code.py
+++ code.py
@@ -20,33 +20,36 @@ movement_motors = motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
 target_reflection = 50 # adjust needs to be the average between white n black
 KP = 1.0
 base_speed = 40
-reflection = color_sensor.reflection(port.B)
+colour = color_sensor.color(port.B)
 
-while True: 
-    motor.run_for_time(port.E, 10000, 500) # can knocker
-    error = reflection - target_reflection
-    turn_rate = int(KP * error)
-    if distance_sensor.distance(port.A) < 150: # detect when the distance is less than 15 cm
-            detection_count += 1
-            if detection_count > 5: # only beeps (substitute for quack sound) when the detection_count is above 5
-                sound.play('Squeaky Toy', 80, 50)
-                detection_count = 0 # resets the detection_count
-
-    
-    if 30 < reflection < 60: #needs adjustment, this is for green square counter 
-        if not was_on_green:
+async def main():
+    global detection_count, was_on_green, score
+    while colour != color.RED:
+        motor.run_for_time(port.E, 10000, 500) # can knocker
+        if colour == color.BLACK:
+            motor_pair.move(motor_pair.PAIR_1, 75)
+        elif colour == color.GREEN: #needs adjustment, this is for green square counter
+            if not was_on_green:
                 score += 1
                 light_matrix.write(str(score))
-                #keeps moving after it senses green
-                motor_pair.move(motor_pair.PAIR_1, turn_rate, velocity=base_speed)
                 was_on_green = True
+                #keeps moving after it senses green
+                motor_pair.move(motor_pair.PAIR_1, -75)
                 #stops after sensing a certain amount of green squares
                 if score == 11:
                     light_matrix.clear()
-    
-    if < reflection < : # if the colour sensed is red it will stop and play a success tune
+                    motor_pair.move(motor_pair.PAIR_1, 0)        
+        else:
+            motor_pair.move(motor_pair.PAIR_1, -75)
+
+        if distance_sensor.distance(port.A) < 150: # detect when the distance is less than 15 cm
+            detection_count += 1
+            if detection_count > 5:
+                sound.play('Squeaky Toy', 80, 50)
+                detection_count = 0 # resets the detection_count
+
+    while colour == color.RED:
         motor_pair.stop(motor_pair.PAIR_1)
-        sound.play('Emotional Piano')
-    else:
-        motor_pair.move(motor_pair.PAIR_1, turn_rate, velocity=base_speed)
-    
+        sound.play('Emotional Piano', 80, 50)
+
+runloop.run(main())
