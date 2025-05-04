3c2514c8dd3ac904bcbe19eb039ce2324bd32607
l1sac <meihuan.chen@education.nsw.gov.au>
2025-05-04 18:57:09 +1000

    final????? need to test

--- hardcode.py
+++ hardcode.py
@@ -3,51 +3,59 @@ from motor import velocity
 import runloop
 import motor_pair
 from hub import port
-import color_sensor
-import distance_sensor
-import color
 import motor
-import time
 from app import sound
-import asyncio
 
 motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
 
 async def main():
-    global score
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 3460, 0, velocity=360) # up to first gs
-    motor.run_for_time(port.E, 4845, velocity=360)
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 2076, 0, velocity=360) # up to first can
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200) # remove can
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 1384, 0, velocity=360) # up to first gs
     await light_matrix.write('1')
     await motor_pair.move_for_time(motor_pair.PAIR_1, 1024, 0, velocity=360) # up to middle of first circle
-    motor.run_for_time(port.E, 1418, velocity=360)
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200) # remove can
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, 200, 90, velocity = 200) # turn 90 degrees
     await motor_pair.move_for_time(motor_pair.PAIR_1, 990, 0, velocity=360) # to second gs
     await light_matrix.write('2')
     await motor_pair.move_for_time(motor_pair.PAIR_1, 3954, 0, velocity=360) # to third gs
     await light_matrix.write('3')
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 1829, 0, velocity=360) # to fourth gs
-    motor.run_for_time(port.E, 2560, velocity=360)
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 929, 0, velocity=360) # to third can 
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200) # remove can
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 900, 0, velocity=360) # to fourth gs
     await light_matrix.write('4')
     await motor_pair.move_for_time(motor_pair.PAIR_1, 4745, 0, velocity=360) # to corner
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, 200, 90, velocity = 200) # turn
     await motor_pair.move_for_time(motor_pair.PAIR_1, 2966, 0, velocity=360) # to corner
-    motor.run_for_time(port.E, 4152, velocity=360)
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200)
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, 200, 90, velocity = 200) # turn
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 6228, 0, velocity=360) # to duck
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 4000, 0, velocity=360) # to can
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200) # remove can
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 2228, 0, velocity=360) # to duck
+    await sound.play('Squeaky Toy')
     await motor_pair.move_for_time(motor_pair.PAIR_1, 1829, 0, velocity=360) # to 5th gs
     await light_matrix.write('5')
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 1087, 0, velocity=360), # to middle of second circle
-    motor.run_for_time(port.E, 1418, velocity=360)
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 1087, 0, velocity=360) # to middle of second circle
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200) # remove can
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, 200, 90, velocity = 200) # turn 90 degrees
     await motor_pair.move_for_time(motor_pair.PAIR_1, 989, 0, velocity=360) # to sixth gs
     await light_matrix.write('6')
     await motor_pair.move_for_time(motor_pair.PAIR_1, 2027, 0, velocity=360)
-    motor.run_for_time(port.E, 2837, velocity=360)
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200) # remove can
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, 200, 90, velocity = 200) # turn 90 degrees
     await motor_pair.move_for_time(motor_pair.PAIR_1, 4794, 0, velocity=360) # up to 7th gs
     await light_matrix.write('7')
-    await motor_pair.move_for_time(motor_pair.PAIR_1, 2298, 0, velocity=360) # up to 8th gs
-    motor.run_for_time(port.E, 3218, velocity=360)
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 1298, 0, velocity=360) # up to 8th can
+    await motor.run_for_time(port.E, 2000, velocity=360) # knock over can
+    await runloop.sleep_ms(1200) # remove can
+    await motor_pair.move_for_time(motor_pair.PAIR_1, 1000, 0, velocity=360) # up to 8th gs
     await light_matrix.write('8')
     await motor_pair.move_for_time(motor_pair.PAIR_1, 2990, 0, velocity=360) # up to red end
     await sound.play('Emotional Piano')
